d89ebf20a67bece5a2b3b2535dded694e156083f
Jon Hall <jon@flowmo.co>
2016-11-20 15:23:27 +0000

    Initial set up; Typescript compiles but throws a couple of errors

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Hello from "./Components/Hello"
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Hello name="Jon" />
+        )
+    }
+}
