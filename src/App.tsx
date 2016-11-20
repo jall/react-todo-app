bd593e253ce831ff9bed98a146d655f379ebd1cc
Jon Hall <jon@flowmo.co>
2016-11-20 15:34:55 +0000

    Fix last Typescript errors (missing paramter types for App)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Hello from "./Components/Hello"
 
-export default class App extends React.Component {
+export default class App extends React.Component<any, any> {
     render() {
         return (
             <Hello name="Jon" />
