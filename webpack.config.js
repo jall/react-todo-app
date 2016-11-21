4795ae75e47d3601e53f3e69d3ecdfc08cd5f3f5
Jon Hall <jon@flowmo.co>
2016-11-21 20:11:10 +0000

    Add basic stylesheet & set up importing css modules

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,14 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".css"]
     },
 
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
         ],
 
         preLoaders: [
